3454ddc78181a63e492236f88b1037a9f53b3a81
Edgar Francis Caballero <efrcaballero@gmail.com>
2022-03-07 15:29:04 -0700

    added files

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <AppHeader />
+    <section>
+      <h2>Page Content</h2>
+      <p>This section is directly on the index.vue file</p>
+      <p><strong>Edgar Caballero</strong></p>
+    </section>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
